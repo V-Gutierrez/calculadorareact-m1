812ab0f336647bccb0809df3aa7b7fb80846ece1
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2020-05-03 22:03:56 -0400

    Updates

---
+++ precache-manifest.35445f9cc6789ec80c4d39c6d904208b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44b30bf3238fe89d3633ff99f91447b",
+    "url": "/calculadorareact-m1/index.html"
+  },
+  {
+    "revision": "2d48c30514373cd46fa3",
+    "url": "/calculadorareact-m1/static/css/main.40fc0dd4.chunk.css"
+  },
+  {
+    "revision": "3d0914231149e0ae055c",
+    "url": "/calculadorareact-m1/static/js/2.a5eca882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculadorareact-m1/static/js/2.a5eca882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d48c30514373cd46fa3",
+    "url": "/calculadorareact-m1/static/js/main.a696c846.chunk.js"
+  },
+  {
+    "revision": "175a5e6a17f384299884",
+    "url": "/calculadorareact-m1/static/js/runtime-main.b93ca231.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculadorareact-m1/static/media/logo.5d5d9eef.svg"
+  }
+]);
